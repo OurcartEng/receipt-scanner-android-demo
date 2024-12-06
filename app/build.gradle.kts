4020f10851cb7fbdc9feafe8e58549a8ec186052
Adrian Redzik <adrian.redzik@ourcart.com>
2024-12-06 10:54:50 +0100

    1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("com.ourcart:receiptscanner:1.0.7")
+    implementation("com.ourcart:receiptscanner:1.0.8")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.navigation:navigation-fragment:2.6.0")
